357454c183d91ca4c0d291273923077ab531724b
jihuacao <CJHdream@outlook.com>
2024-01-12 22:58:02 +0800

    update

---
+++ cjh/from_nvidia-dev.sh
@@ -0,0 +1,9 @@
+apt update
+apt -y install libopencv-dev
+pip3 install pyproject==1.3.1 wheel==0.40.0
+pip3 install openmim==0.3.9
+pip3 install mmcv==2.1.0
+pip3 install mmdet=3.3.0
+pip3 install mmengine==0.10.2
+pip3 install einops==0.7.0
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
