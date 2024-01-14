994ff56887bf75a5b43d0d4955118d3a9472d3ce
jihuacao <CJHdream@outlook.com>
2024-01-14 13:39:47 +0800

    update

---
+++ cjh/export_stgcn.sh
@@ -0,0 +1,4 @@
+PYTHONAPTH=$(pwd) python tools/deployment/export_onnx_gcn.py \
+configs/skeleton/stgcn/stgcn_8xb16-joint-u100-80e_ntu120-xsub-keypoint-2d.py \
+stgcn_8xb16-joint-u100-80e_ntu120-xsub-keypoint-2d_20221129-612416c6.pth \
+--output_file=stgcn_8xb16-joint-u100-80e_ntu120-xsub-keypoint-2d_20221129-612416c6.onnx"
